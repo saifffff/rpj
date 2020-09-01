e6e219d183194c4fb71f51c9e48bbebf0d2af3fb
saifffff <angryn1nj4@gmail.com>
2020-09-01 01:41:42 -0700

    Updates

---
+++ precache-manifest.6543073637d3207f5942d62a82345ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2037b7e37670a028a4f3b341d9460b8",
+    "url": "/rpj/index.html"
+  },
+  {
+    "revision": "b4aca2ac5854b936584b",
+    "url": "/rpj/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "01d40d558916dd1998ab",
+    "url": "/rpj/static/css/main.0d1a01c0.chunk.css"
+  },
+  {
+    "revision": "b4aca2ac5854b936584b",
+    "url": "/rpj/static/js/2.03cb8a4b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rpj/static/js/2.03cb8a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d40d558916dd1998ab",
+    "url": "/rpj/static/js/main.f3815daa.chunk.js"
+  },
+  {
+    "revision": "9f19ae00bf617c3c4b50",
+    "url": "/rpj/static/js/runtime-main.25459d75.js"
+  }
+]);
